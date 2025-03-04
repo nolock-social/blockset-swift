13351a071ecd9984c5ce815dc35daae89d09b17b
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-03-04 10:42:13 -0800

    tests

--- Sources/BlockSet/BlockSet.swift
+++ Sources/BlockSet/BlockSet.swift
@@ -1,4 +1,5 @@
 extension UInt8 {
+    // https://en.wikipedia.org/wiki/Base32#Crockford's_Base32
     private static let u5ToChar: [Character] = [
         "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f",
         "g", "h", "j", "k", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z",
@@ -9,6 +10,48 @@ extension UInt8 {
 }
 
 private struct Remainder {
-    var value: UInt8
-    var length: UInt8
+    private var value: UInt16
+    private var length: UInt8
+    public static func new() -> Remainder {
+        Remainder(value: 0, length: 0)
+    }
+    public mutating func push(_ value: UInt8) {
+        self.value |= UInt16(value) << (8 - self.length)
+        self.length += 8
+    }
+    public mutating func pop() -> Character? {
+        if self.length < 5 {
+            return nil
+        }
+        let result = self.base32()
+        self.value <<= 5
+        self.length -= 5
+        return result
+    }
+    public func last() -> Character? {
+        if self.length == 0 {
+            return nil
+        }
+        return self.base32()
+    }
+    private func base32() -> Character {
+        UInt8(self.value >> 11).base32()
+    }
+}
+
+extension Sequence<UInt8> {
+    public func base32() -> String {
+        var remainder = Remainder.new()
+        var result = ""
+        for byte in self {
+            remainder.push(byte)
+            while let value = remainder.pop() {
+                result.append(value)
+            }
+        }
+        if let value = remainder.last() {
+            result.append(value)
+        }
+        return result
+    }
 }
--- Tests/BlockSetTests/BlockSetTests.swift
+++ Tests/BlockSetTests/BlockSetTests.swift
@@ -7,4 +7,7 @@ import Testing
     #expect(UInt8(10).base32() == "a")
     #expect(UInt8(15).base32() == "f")
     #expect(UInt8(31).base32() == "z")
+    //
+    let x: [UInt8] = [0b1011_0111, 0b010_10010, 0b1011_1001]
+    #expect(x.base32() == "px9bj")
 }
