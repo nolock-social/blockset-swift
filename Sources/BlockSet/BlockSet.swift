50aff9f82ebcfa985a5c669cca10af4d1e851dff
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-03-04 13:55:06 -0800

    ok

--- Sources/BlockSet/BlockSet.swift
+++ Sources/BlockSet/BlockSet.swift
@@ -4,12 +4,51 @@ extension UInt8 {
         "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f",
         "g", "h", "j", "k", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z",
     ]
-    public func base32() -> Character {
+    func base32() -> Character {
         Self.u5ToChar[Int(self)]
     }
 }
 
-private struct Remainder {
+private protocol BitSplitState {
+    static var inputBits: UInt8 { get }
+    static var outputBits: UInt8 { get }
+}
+
+private struct State<S: BitSplitState> {
+    // private:
+    private var value: UInt16 = 0
+    private var length: UInt8 = 0
+    private func output() -> UInt8 {
+        UInt8(self.value >> (16 - S.outputBits))
+    }
+    // public:
+    mutating func push(_ value: UInt8) {
+        self.value |= UInt16(value) << (16 - S.inputBits - self.length)
+        self.length += S.inputBits
+    }
+    mutating func pop() -> UInt8? {
+        guard self.length >= S.outputBits else {
+            return nil
+        }
+        let result = self.output()
+        self.value <<= S.outputBits
+        self.length -= S.outputBits
+        return result
+    }
+    func last() -> UInt8? {
+        guard self.length != 0 else {
+            return nil
+        }
+        return self.output()
+    }
+}
+
+struct Bit8To5: BitSplitState {
+    static let inputBits: UInt8 = 8
+    static let outputBits: UInt8 = 5
+}
+
+private struct U8ToCharState {
     // private:
     private var value: UInt16 = 0
     private var length: UInt8 = 0
@@ -17,11 +56,11 @@ private struct Remainder {
         UInt8(self.value >> 11).base32()
     }
     // public:
-    public mutating func push(_ value: UInt8) {
+    mutating func push(_ value: UInt8) {
         self.value |= UInt16(value) << (8 - self.length)
         self.length += 8
     }
-    public mutating func pop() -> Character? {
+    mutating func pop() -> Character? {
         guard self.length >= 5 else {
             return nil
         }
@@ -30,7 +69,7 @@ private struct Remainder {
         self.length -= 5
         return result
     }
-    public func last() -> Character? {
+    func last() -> Character? {
         guard self.length != 0 else {
             return nil
         }
@@ -40,15 +79,15 @@ private struct Remainder {
 
 extension Sequence<UInt8> {
     public func base32() -> String {
-        var remainder = Remainder()
+        var state = U8ToCharState()
         var result = ""
         for byte in self {
-            remainder.push(byte)
-            while let value = remainder.pop() {
+            state.push(byte)
+            while let value = state.pop() {
                 result.append(value)
             }
         }
-        if let value = remainder.last() {
+        if let value = state.last() {
             result.append(value)
         }
         return result
