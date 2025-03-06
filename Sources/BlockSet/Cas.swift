d2370c7f91c8a4597c74b0b2502f2b7b59dbe70e
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-03-06 12:41:29 -0800

    resolve packages

--- Sources/BlockSet/Cas.swift
+++ Sources/BlockSet/Cas.swift
@@ -5,17 +5,3 @@ protocol Cas {
     func add(_ block: Data) -> String?
     func get(_ id: String) -> Data?
 }
-
-struct BlockSet: Cas {
-    private var blocks: [String: Data] = [:]
-
-    func add(_ block: Data) -> String? {
-        let id = block.sha256().base32()
-        blocks[id] = block
-        return id
-    }
-
-    func get(_ id: String) -> Data? {
-        blocks[id]
-    }
-}
---
+++ Sources/BlockSet/FileCas.swift
@@ -0,0 +1,13 @@
+struct FileCas: Cas {
+    private var blocks: [String: Data] = [:]
+
+    func add(_ block: Data) -> String? {
+        let id = block.sha256().base32()
+        blocks[id] = block
+        return id
+    }
+
+    func get(_ id: String) -> Data? {
+        blocks[id]
+    }
+}
