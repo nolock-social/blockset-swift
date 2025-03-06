7405e8c4aaae30fcea53ba7fdf50350728649ea1
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-03-06 15:28:59 -0800

    ok

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,6 @@
 cd ~/
-wget https://download.swift.org/swift-6.0.3-release/ubuntu2404-aarch64/swift-6.0.3-RELEASE/swift-6.0.3-RELEASE-ubuntu24.04-aarch64.tar.gz
-tar xzf swift-6.0.3-RELEASE-ubuntu24.04-aarch64.tar.gz
-export PATH=~/swift-6.0.3-RELEASE-ubuntu24.04-aarch64/usr/bin:"${PATH}"
+export SWIFT_DIR=swift-6.0.3-RELEASE-ubuntu24.04-aarch64
+export SWIFT_TAR_GZ=${SWIFT_DIR}.tar.gz
+wget https://download.swift.org/swift-6.0.3-release/ubuntu2404-aarch64/swift-6.0.3-RELEASE/${SWIFT_TAR_GZ}
+tar xzf ${SWIFT_TAR_GZ}
+export PATH=~/${SWIFT_DIR}/usr/bin:"${PATH}"
