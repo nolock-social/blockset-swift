167f6224a098a1b8bfec8f69bd1932f9a1eb9148
Kodi Maberek <138794715+KodiMaberek@users.noreply.github.com>
2025-04-02 10:14:07 -0700

    Add codable to model

--- Sources/BlockSet/Model.swift
+++ Sources/BlockSet/Model.swift
@@ -3,26 +3,43 @@ struct ModelStruct<T> {
     var value: T
 }
 
-public class Model<T>: Hashable {
-    // internal:
+public class Model<T: Codable>: Hashable, Codable {
     var s: ModelStruct<T>
+
     init(_ s: ModelStruct<T>) {
         self.s = s
     }
-    // public:
+
     public static func initial(_ value: T) -> Model {
         Model(ModelStruct(mutable: Mutable.initial(), value: value))
     }
+
     public var value: T {
         get { s.value }
         set { s.value = newValue }
     }
 
-    // Hashable:
+    // MARK: - Codable
+    enum CodingKeys: String, CodingKey {
+        case value
+    }
 
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let decodedValue = try container.decode(T.self, forKey: .value)
+        self.s = ModelStruct(mutable: Mutable.initial(), value: decodedValue)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(s.value, forKey: .value)
+    }
+
+    // MARK: - Hashable
     public static func == (lhs: Model, rhs: Model) -> Bool {
         return lhs === rhs
     }
+
     public func hash(into hasher: inout Hasher) {
         hasher.combine(ObjectIdentifier(self))
     }
@@ -30,10 +47,10 @@ public class Model<T>: Hashable {
 
 extension Cas {
     @discardableResult
-    public func saveJsonModel<T: Encodable>(_ model: Model<T>) throws -> String? {
+    public func saveJsonModel<T: Codable>(_ model: Model<T>) throws -> String? {
         try saveJson(model.s.mutable, model.s.value)
     }
-    public func loadJsonModel<T: Decodable>(_ mutable: Mutable) throws -> Model<T>? {
+    public func loadJsonModel<T: Codable>(_ mutable: Mutable) throws -> Model<T>? {
         guard let value: T = try loadJson(mutable) else {
             return nil
         }
