42ae21d576aae95c28224a2705d93fb064450a30
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-04-03 10:41:25 -0700

    multi-cas

---
+++ Sources/BlockSet/MultiCas.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+public class MultiCas: Cas {
+    private var local: Cas
+    private var remote: Cas
+
+    public init(local: Cas, remote: Cas) {
+        self.local = local
+        self.remote = remote
+    }
+
+    public func id(_ data: Data) -> String {
+        local.id(data)
+    }
+
+    public func add(_ data: Data) throws -> String {
+        let id = try local.add(data)
+        do {
+            try remote.add(data)
+        } catch {
+        }
+        return id
+    }
+
+    public func get(_ id: String) throws -> Data? {
+        if let data = try local.get(id) {
+            return data
+        }
+        do {
+            if let data = try remote.get(id) {
+                try local.add(data)
+                return data
+            }
+        } catch {
+        }
+        return nil
+    }
+
+    public func list() throws -> AnySequence<String> {
+        try local.list()
+    }
+
+    public func syncRemote() throws {
+        try local.sync(remote)
+    }
+}
