7446497e8c8212502182ecd57022e14231a33a7e
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-05-10 18:05:25 -0700

    Html

--- Sources/BlockSet/Html.swift
+++ Sources/BlockSet/Html.swift
@@ -19,6 +19,20 @@ public func e(_ name: String, _ children: Child...) -> Child {
 
 //
 
+func str(_ s: String) -> String {
+    return s
+        .replacingOccurrences(of: "&", with: "&amp;")
+        .replacingOccurrences(of: "<", with: "&lt;")
+        .replacingOccurrences(of: ">", with: "&gt;")
+        .replacingOccurrences(of: "\"", with: "&quot;")
+}
+
+typealias Attribute = (key: String, value: String)
+
+func str(_ a: Attribute) -> String {
+    return " \(a.key)=\"\(str(a.value))\""
+}
+
 let voidTags: Set<String> = [
     "area",
     "base",
@@ -30,11 +44,30 @@ let voidTags: Set<String> = [
     "input",
     "link",
     "meta",
+    "param",
     "source",
     "track",
     "wbr",
 ]
 
-public func toString(_ e: Element) -> String {
-    ""
+func str(_ c: Child) -> String {
+    switch c {
+    case .e(let e):
+        return str(e)
+    case .t(let t):
+        return str(t)
+    }
+}
+
+func str(_ c: [Child]) -> String {
+    return c.map(str).joined()
+}
+
+func str(_ e: Element) -> String {
+    let body = voidTags.contains(e.name) ? "" : str(e.children) + "</\(e.name)>"
+    return "<\(e.name)" + e.attributes.map(str).joined() + ">" + body
+}
+
+func html(_ e: Element) -> String {
+    return "<!DOCTYPE html>" + str(e)
 }
