92a138e17c0894c81a01f46eb8b77f66a83317a2
Kodi Maberek <138794715+KodiMaberek@users.noreply.github.com>
2025-05-12 10:48:21 -0700

    Change model of reciept

--- Sources/BlockSet/ReceiptModel.swift
+++ Sources/BlockSet/ReceiptModel.swift
@@ -1,23 +1,29 @@
 import Foundation
 
+public struct LocationModel: Codable, Hashable {
+    public var latitude: Double = 0
+    public var longitude: Double = 0
+
+    public init() {}
+}
+
 public struct ImageModel: Codable, Hashable {
     public var image: String = ""
-    public var date: String = ""
-    public var location: String = ""
+    public var date: Double = 0
+    public var location: LocationModel = LocationModel()
 
     public init() {}
 }
 
 public struct ReceiptModel: Codable, Hashable {
     public var image: ImageModel = ImageModel()
-    public var price: Double? = nil
+    public var price: String = ""
     public var title: String = ""
     public var description: String = ""
     public var imageList: [String]? = nil
-    public var owner: String = "Personal space"
 
-    public var date: String = ""
-    public var location: String = ""
+    public var date: Double = 0
+    public var location: LocationModel = LocationModel()
 
     public init() {}
 }
