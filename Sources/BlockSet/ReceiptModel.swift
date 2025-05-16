c9ae11299c122c0bbda3839ee88ccc92f5031ad7
Kodi Maberek <138794715+KodiMaberek@users.noreply.github.com>
2025-05-16 11:55:21 -0700

    Update default value for RecieptModel

--- Sources/BlockSet/ReceiptModel.swift
+++ Sources/BlockSet/ReceiptModel.swift
@@ -1,15 +1,8 @@
 import Foundation
 
-public struct LocationModel: Codable, Hashable {
-    public var latitude: Double = 0
-    public var longitude: Double = 0
-
-    public init() {}
-}
-
 public struct ImageModel: Codable, Hashable {
-    public var image: String = ""
-    public var date: Double = 0
+    public var image: String = String()
+    public var date: Double = Double()
     public var location: LocationModel = LocationModel()
 
     public init() {}
@@ -17,10 +10,10 @@ public struct ImageModel: Codable, Hashable {
 
 public struct ReceiptModel: Codable, Hashable {
     public var image: ImageModel = ImageModel()
-    public var price: Double = 00
-    public var title: String = ""
-    public var description: String = ""
-    public var notes: String = ""
+    public var price: Double = Double()
+    public var title: String = String()
+    public var description: String = String()
+    public var notes: String = String()
     public var imageList: [String]? = nil
 
     public var date: String = ""
