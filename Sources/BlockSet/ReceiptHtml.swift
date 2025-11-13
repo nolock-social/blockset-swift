30b5a853ae3f921418a83fb8b8a91efc2ef6f686
Kodi Maberek <138794715+KodiMaberek@users.noreply.github.com>
2025-11-13 12:45:20 -0800

    upd html generator

--- Sources/BlockSet/ReceiptHtml.swift
+++ Sources/BlockSet/ReceiptHtml.swift
@@ -1,44 +1,54 @@
 import Foundation
 
 extension ReceiptModel {
-    public func toHtml() -> Child {
-        return e(
-            "table", [("class", "check-table")],
+public func toHtml() -> Child {
+    let dateString: String = {
+        guard let timestamp = date else { return "" }
+        return Date(timeIntervalSince1970: timestamp).convertToString()
+    }()
+
+    let priceString: String = {
+        guard let priceValue = price else { return "" }
+        return String(format: "%.2f", priceValue)
+    }()
+
+    return e(
+        "table", [("class", "check-table")],
+        e(
+            "tr",
+            e("th", [("colspan", "2")], .t(title ?? ""))
+        ),
+        e(
+            "tr",
+            e("td", [("colspan", "2")], .t(description ?? ""))
+        ),
+        e(
+            "tr",
+            e("th", .t("Total:")),
+            e("th", .t(priceString))
+        ),
+        e(
+            "tr",
+            e("td", .t("Date:")),
+            e("td", .t(dateString))
+        ),
+        e(
+            "tr",
+            e("td", .t("Location:")),
+            e("td", .t(location?.formattedAddress ?? ""))
+        ),
+        e(
+            "tr",
             e(
-                "tr",
-                e("th", [("colspan", "2")], .t("\(title)"))
-            ),
-            e(
-                "tr",
-                e("td", [("colspan", "2")], .t("\(description)"))
-            ),
-            e(
-                "tr",
-                e("th", .t("Total:")),
-                e("th", .t("\(price)"))
-            ),
-            e(
-                "tr",
-                e("td", .t("Date:")),
-                e("td", .t("\(date)"))
-            ),
-            e(
-                "tr",
-                e("td", .t("Location:")),
-                e("td", .t("\(location?.formattedAddress)"))
-            ),
-            e(
-                "tr",
-                e(
-                    "td", [("colspan", "2"), ("class", "img-wrap")],
-                    e("img", [
-                        ("src", "content/\(image).jpg"),
-                        ("alt", "")
-                    ])
-                )
+                "td", [("colspan", "2"), ("class", "img-wrap")],
+                e("img", [
+                    ("src", "content/\(image ?? "").jpg"),
+                    ("alt", "")
+                ])
             )
         )
-    }
+    )
+}
 }
 
 let styleCss = try! String(
@@ -63,5 +73,18 @@ extension [ReceiptModel] {
             )
         )
         return html(node)
+    }
+}
+
+extension Date {
+    func convertToString() -> String {
+        let f = DateFormatter()
+        f.dateFormat = "M/dd/yy, h:mma, EEEE"
+        f.amSymbol = "am"
+        f.pmSymbol = "pm"
+        f.locale = Locale.current
+        f.timeZone = .current
+
+        return f.string(from: self)
     }
 }
