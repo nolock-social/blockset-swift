ba97d1efbb628c1b3f74696178caa10a0fe43158
Kodi Maberek <138794715+KodiMaberek@users.noreply.github.com>
2025-11-13 11:24:39 -0800

    upd receiptHtml

--- Sources/BlockSet/ReceiptHtml.swift
+++ Sources/BlockSet/ReceiptHtml.swift
@@ -1,44 +1,44 @@
 import Foundation
 
 extension ReceiptModel {
-    // public func toHtml() -> Child {
-    //     return e(
-    //         "table", [("class", "check-table")],
-    //         e(
-    //             "tr",
-    //             e("th", [("colspan", "2")], .t(title))
-    //         ),
-    //         e(
-    //             "tr",
-    //             e("td", [("colspan", "2")], .t(description))
-    //         ),
-    //         e(
-    //             "tr",
-    //             e("th", .t("Total:")),
-    //             e("th", .t("\(price)"))
-    //         ),
-    //         e(
-    //             "tr",
-    //             e("td", .t("Date:")),
-    //             e("td", .t("\(date)"))
-    //         ),
-    //         e(
-    //             "tr",
-    //             e("td", .t("Location:")),
-    //             e("td", .t(location))
-    //         ),
-    //         e(
-    //             "tr",
-    //             e(
-    //                 "td", [("colspan", "2"), ("class", "img-wrap")],
-    //                 e("img", [
-    //                     ("src", "content/\(image.image).jpg"),
-    //                     ("alt", "")
-    //                 ])
-    //             )
-    //         )
-    //     )
-    // }
+    public func toHtml() -> Child {
+        return e(
+            "table", [("class", "check-table")],
+            e(
+                "tr",
+                e("th", [("colspan", "2")], .t("\(title)"))
+            ),
+            e(
+                "tr",
+                e("td", [("colspan", "2")], .t("\(description)"))
+            ),
+            e(
+                "tr",
+                e("th", .t("Total:")),
+                e("th", .t("\(price)"))
+            ),
+            e(
+                "tr",
+                e("td", .t("Date:")),
+                e("td", .t("\(date)"))
+            ),
+            e(
+                "tr",
+                e("td", .t("Location:")),
+                e("td", .t("\(location?.formattedAddress)"))
+            ),
+            e(
+                "tr",
+                e(
+                    "td", [("colspan", "2"), ("class", "img-wrap")],
+                    e("img", [
+                        ("src", "content/\(image).jpg"),
+                        ("alt", "")
+                    ])
+                )
+            )
+        )
+    }
 }
 
 let styleCss = try! String(
@@ -46,22 +46,22 @@ let styleCss = try! String(
     encoding: .utf8
 )
 
-// extension [ReceiptModel] {
-    // public func toHtml() -> String {
-        // let node = e(
-        //     "html",
-        //     e(
-        //         "head",
-        //         e("meta", [("name", "viewport"), ("content", "width=device-width, initial-scale=1.0")]),
-        //         e("title", .t("Receipt List")),
-        //         e("style", [("type", "text/css")], .t(styleCss))
-        //     ),
-        //     e("body",
-        //         .e(("div", [("class", "container")],
-        //             self.map { $0.toHtml() }
-        //         ))
-        //     )
-        // )
-        // return html(node)
-    // }
-// }
+extension [ReceiptModel] {
+    public func toHtml() -> String {
+        let node = e(
+            "html",
+            e(
+                "head",
+                e("meta", [("name", "viewport"), ("content", "width=device-width, initial-scale=1.0")]),
+                e("title", .t("Receipt List")),
+                e("style", [("type", "text/css")], .t(styleCss))
+            ),
+            e("body",
+                .e(("div", [("class", "container")],
+                    self.map { $0.toHtml() }
+                ))
+            )
+        )
+        return html(node)
+    }
+}
